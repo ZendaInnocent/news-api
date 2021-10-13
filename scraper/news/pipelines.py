2af6dd685c9deafaaad06af6e497627e23ae6bca
Innocent Zenda <zendainnocent@gmail.com>
2021-10-13 09:23:41 +0300

    scraper: setup

---
+++ scraper/news/pipelines.py
@@ -0,0 +1,9 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
